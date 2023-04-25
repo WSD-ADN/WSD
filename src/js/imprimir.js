3d05cb3559267ceb8fe5a6f3046853acd6a4dd86
Juan Carlo Cubero Chaves <juancarlogym@gmail.com>
2023-04-24 19:04:30 -0600

    Agregar mejoras y PDF Curriculum

---
+++ src/js/imprimir.js
@@ -0,0 +1,33 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    //  click del botón
+    const $boton = document.querySelector("#btnCrearPdf");
+    if ($boton) {
+      $boton.addEventListener("click", () => {
+        const cv = document.getElementById("cv-pdf");
+  
+        const $elementoParaConvertir = cv; //Elemento del DOM
+        html2pdf()
+          .set({
+            margin: 1,
+            filename: "documento.pdf",
+            image: {
+              type: "jpeg",
+              quality: 1.0,
+            },
+            html2canvas: {
+              scale: 4, // A mayor escala, mejores gráficos, pero más peso
+              letterRendering: true,
+            },
+            jsPDF: {
+              unit: "in",
+              format: "a3",
+              orientation: "portrait", // landscape o portrait
+            },
+          })
+          .from($elementoParaConvertir)
+          .save()
+          .catch((err) => console.log(err));
+      });
+    }
+  });
