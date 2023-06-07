971366d22173d3ad02b1aca8ef840fe976c8a8d5
Juan Carlo Cubero Chaves <juancarlogym@gmail.com>
2023-06-07 16:55:47 -0600

    modificar el portafolio

--- src/js/carrucel.js
+++ src/js/carrucel.js
@@ -1,20 +1,30 @@
 // Obtén el contenedor y las imágenes
-const infoContainer = document.getElementById('infoContainer');
-const images = infoContainer.getElementsByTagName('img');
+const infoContainers = document.querySelectorAll('.info-container');
+
 // Establece el índice de la imagen actual
 let currentIndex = 0;
 // Establece el intervalo de tiempo (en milisegundos) para cambiar las imágenes
 const intervalTime = 3500; // Cambia cada 3 segundos
 
-// Función para cambiar la imagen
+// ...
 function changeImage() {
-    // Oculta la imagen actual
-    images[currentIndex].style.opacity = '0';
-    // Incrementa el índice de la imagen actual
-    currentIndex = (currentIndex + 1) % images.length;
-    // Muestra la nueva imagen
-    images[currentIndex].style.opacity = '1';
-}
+    infoContainers.forEach(infoContainer => {
+      const images = infoContainer.getElementsByTagName('img');
+  
+      // Verificar si hay imágenes antes de continuar
+      if (images.length > 0 && images[currentIndex]) {
+        // Ocultar la imagen actual
+        images[currentIndex].style.opacity = '0';
+        // Incrementar el índice de la imagen actual
+        currentIndex = (currentIndex + 1) % images.length;
+        // Mostrar la nueva imagen
+        images[currentIndex].style.opacity = '1';
+      }
+    });
+  }
+  
+// Función para cambiar la imagen
+
 
 // Inicia el carrusel
 const intervalId = setInterval(changeImage, intervalTime);
