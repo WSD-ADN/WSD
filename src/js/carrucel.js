fa91950b2f07e1fe73a981bcf9b05e21f6914bef
Juan Carlo Cubero Chaves <juancarlogym@gmail.com>
2023-06-12 19:36:36 -0600

    Actualización agregar un proyecto

--- src/js/carrucel.js
+++ src/js/carrucel.js
@@ -1,32 +1,34 @@
-// Obtén el contenedor y las imágenes
+// Obtén los contenedores de imágenes
 const infoContainers = document.querySelectorAll('.info-container');
 
-// Establece el índice de la imagen actual
-let currentIndex = 0;
 // Establece el intervalo de tiempo (en milisegundos) para cambiar las imágenes
-const intervalTime = 3500; // Cambia cada 3 segundos
+const intervalTime = 3000; // Cambia cada 3 segundos
 
-// ...
-function changeImage() {
-    infoContainers.forEach(infoContainer => {
-      const images = infoContainer.getElementsByTagName('img');
-  
-      // Verificar si hay imágenes antes de continuar
-      if (images.length > 0 && images[currentIndex]) {
-        // Ocultar la imagen actual
-        images[currentIndex].style.opacity = '0';
-        // Incrementar el índice de la imagen actual
-        currentIndex = (currentIndex + 1) % images.length;
-        // Mostrar la nueva imagen
-        images[currentIndex].style.opacity = '1';
-      }
-    });
-  }
-  
 // Función para cambiar la imagen
+function changeImage() {
+  infoContainers.forEach(infoContainer => {
+    const images = infoContainer.querySelectorAll('img');
+    // Verificar si hay imágenes antes de continuar
+    if (images.length > 0) {
+      // Ocultar la imagen actual
+      images.forEach(image => {
+        image.style.opacity = '0';
+      });
+      // Incrementar el índice de la imagen actual
+      infoContainer.currentIndex = (infoContainer.currentIndex + 1) % images.length;
+      // Mostrar la nueva imagen
+      images[infoContainer.currentIndex].style.opacity = '1';
+    }
+  });
+}
 
+// Inicia el carrusel para cada contenedor de imágenes
+infoContainers.forEach(infoContainer => {
+  infoContainer.currentIndex = 0; // Establece el índice de la imagen actual
+});
 
 // Inicia el carrusel
 const intervalId = setInterval(changeImage, intervalTime);
+
 
 
