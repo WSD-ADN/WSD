2cbe5b1ceff347f7c74ae5cd36e43d8cb7d206ac
Juan Carlo Cubero Chaves <juancarlogym@gmail.com>
2023-06-13 15:47:24 -0600

    Actualización 13/6/23 numero 2 ajustes

--- src/js/script.js
+++ src/js/script.js
@@ -32,99 +32,115 @@ type();
 
 //------------- Navegacion -------------------
 
-const nav = document.querySelector('.nav'),
-      navList = nav.querySelectorAll('li'),
-      totalNavList = navList.length,
-      allSection = document.querySelectorAll('.section'),
-      totalSection = allSection.length;
-      for(let i=0; i<totalNavList; i++){
-        const a = navList[i].querySelector('a');
-        a.addEventListener('click', function(){
+
+ const nav = document.querySelector('.nav'),
+       navList = nav.querySelectorAll('li'),
+       totalNavList = navList.length,
+       allSection = document.querySelectorAll('.section'),
+       totalSection = allSection.length;
+       for(let i=0; i<totalNavList; i++){
+         const a = navList[i].querySelector('a');
+         a.addEventListener('click', function(){
 
           
-          removeBackSection();
-          for(let x=0 ; x<totalNavList; x++){
-            if(navList[x].querySelector('a').classList.contains('active')){
-              addBackSection(x);
-            //  allSection[x].classList.add('back-section');
-            }
-            navList[x].querySelector('a').classList.remove('active');
-          }
-          this.classList.add('active');
-          showSection(this);
-          // if(window.innerWidth < 1200){
-          //   asideSectionTogglerBtn();
-          // }
-        });
-      }
-
-      function removeBackSection(){
-          for(let i = 0; i<totalSection; i++){
-              allSection[i].classList.remove('back-section');
-          }
-      }
-      function addBackSection(num){
-        allSection[num].classList.add('back-section');
-      }
-      function showSection(element){
-        for(let i = 0; i<totalSection; i++){
-          allSection[i].classList.remove('active');
-        }
-        const target = element.getAttribute('href').split('#')[1];
-        document.querySelector('#' + target).classList.add('active');
-        
-      }
-// navegar con el btn de contactar //--------------------------------
-
-      function updateNav(element){
-        for(i= 0; i<totalNavList; i++){
-          navList[i].querySelector('a').classList.remove('active');
-          const target = element.getAttribute('href').split('#')[1];
-          if(target === navList[i].querySelector('a').getAttribute('href').split('#')[1]){
-            navList[i].querySelector('a').classList.add('active');
-          }
-        }
-      }
-
-      document.querySelector('.hire-me').addEventListener('click', function(){
-        const sectionIndex = this.getAttribute('data-section-index');
-        showSection(this);
-        updateNav(this);
-        removeBackSection();
-        addBackSection(sectionIndex);
-      })
-//-------------------------------------------------------------------
-      const navTogglerBtn = document.querySelector('.nav-toggler'),
-            aside = document.querySelector('.aside'),
-            section = document.querySelectorAll('.section');
-            navTogglerBtn.addEventListener('click', () =>{
+           removeBackSection();
+           for(let x=0 ; x<totalNavList; x++){
+             if(navList[x].querySelector('a').classList.contains('active')){
+               addBackSection(x);
+               allSection[x].classList.add('back-section');
+             }
+             navList[x].querySelector('a').classList.remove('active');
+           }
+           this.classList.add('active');
+           showSection(this);
+            if(window.innerWidth < 1200){
               asideSectionTogglerBtn();
-            });
-            function asideSectionTogglerBtn() {
-              aside.classList.toggle('open');
-              navTogglerBtn.classList.toggle('open');
-              for (let i = 0; i < section.length; i++) {
-                section[i].classList.toggle('open');
-              }
             }
-//----------------------------------------
+         });
+       }
 
-//----------------------------------------
-//--- Color barra --------------//
-// Obtén la referencia al elemento aside
+       function removeBackSection(){
+           for(let i = 0; i<totalSection; i++){
+               allSection[i].classList.remove('back-section');
+           }
+       }
+       function addBackSection(num){
+         allSection[num].classList.add('back-section');
+       }
+       function showSection(element){
+         for(let i = 0; i<totalSection; i++){
+           allSection[i].classList.remove('active');
+         }
+         const target = element.getAttribute('href').split('#')[1];
+         document.querySelector('#' + target).classList.add('active');
+        
+       }
+// // navegar con el btn de contactar //--------------------------------
 
-var miAside = document.getElementById('miAside');
+       function updateNav(element){
+         for(i= 0; i<totalNavList; i++){
+           navList[i].querySelector('a').classList.remove('active');
+           const target = element.getAttribute('href').split('#')[1];
+           if(target === navList[i].querySelector('a').getAttribute('href').split('#')[1]){
+             navList[i].querySelector('a').classList.add('active');
+           }
+         }
+       }
+
+       document.querySelector('.hire-me').addEventListener('click', function(){
+         const sectionIndex = this.getAttribute('data-section-index');
+         showSection(this);
+         updateNav(this);
+         removeBackSection();
+         addBackSection(sectionIndex);
+       })
+ //-------------------------------------------------------------------
+const navTogglerBtn = document.querySelector('.nav-toggler'),
+      aside = document.querySelector('.aside'),
+      section = document.querySelectorAll('.section');
+      navTogglerBtn.addEventListener('click', () =>{
+        asideSectionTogglerBtn();
+      });
+
+      function asideSectionTogglerBtn() {
+        aside.classList.toggle('open');
+        navTogglerBtn.classList.toggle('open');
+        for (let i = 0; i < section.length; i++) {
+          section[i].classList.toggle('open');
+        }
+      }
+
+      document.addEventListener('click', function (event) {
+       const targetElement = event.target;
+       const windowWidth = window.innerWidth;
+     
+       // Verificar si el ancho de la ventana es mayor o igual a 768px
+       if (windowWidth < 768 && !aside.contains(targetElement) && !nav.contains(targetElement)) {
+         aside.classList.remove('open');
+         navTogglerBtn.classList.remove('open');
+         for (let i = 0; i < section.length; i++) {
+           section[i].classList.remove('open');
+         }
+       }
+     });
+ //----------------------------------------
+
+ //----------------------------------------
+ //--- Color barra --------------//
+ // Obtén la referencia al elemento aside
+
+ const miAside = document.getElementById('miAside');
 
 // Añade un event listener para el evento de hashchange (cambio en el fragmento de URL)
-window.addEventListener('hashchange', function() {
-    // Obtén el fragmento de URL actual
-    var hash = window.location.hash;
+ window.addEventListener('hashchange', function() {
+     // Obtén el fragmento de URL actual
+     let hash = window.location.hash;
 
     // Restaura el estilo por defecto
-    miAside.style.backgroundColor = ''; // Establece el color de fondo a vacío para restaurar el estilo por defecto
-    miAside.style.backgroundImage = 'none'; // Establece la imagen de fondo a none para restaurar el estilo por defecto
-    miAside.style.borderRight = ''; // Establece el borde derecho a vacío para restaurar el estilo por defecto
-    miAside.style.transition = ''; // Restaura la transición a vacío para restaurar el estilo por defecto
+     miAside.style.backgroundColor = ''; // Establece el color de fondo a vacío para restaurar el estilo por defecto
+     miAside.style.backgroundImage = 'none'; // Establece la imagen de fondo a none para restaurar el estilo por defecto
+     miAside.style.borderRight = ''; // Establece el borde derecho a vacío para restaurar el estilo por defecto
+     miAside.style.transition = ''; // Restaura la transición a vacío para restaurar el estilo por defecto
 
     // Si el fragmento de URL es #portafolio, aplica el borde derecho y el color de fondo en el aside con una transición de 1 segundo
     if (hash === '#portafolio') {
@@ -149,3 +165,4 @@ window.addEventListener('hashchange', function() {
 
 
 //../build/img/portafoliofondo.jpg
+
